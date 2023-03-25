acc957fccea7b5e0574bdd2c9d06c95956ee9306
Damian2057 <ddamian2057@gmail.com>
2023-03-25 19:50:23 +0100

    initial structure for the authentication module

---
+++ src/main/java/pl/better/foodzillabackend/auth/controller/AuthenticationController.java
@@ -0,0 +1,9 @@
+package pl.better.foodzillabackend.auth.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthenticationController {
+}
---
+++ src/main/java/pl/better/foodzillabackend/config/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package pl.better.foodzillabackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+}
