5c1d60a2e87e496cdafd52c9f6ee47df8ef41bac
Damian2057 <ddamian2057@gmail.com>
2023-03-26 14:04:45 +0200

    creating initial components for user authorization

--- src/main/java/pl/better/foodzillabackend/security/SecurityConfig.java
+++ src/main/java/pl/better/foodzillabackend/security/SecurityConfig.java
@@ -1,10 +1,8 @@
-package pl.better.foodzillabackend.config;
+package pl.better.foodzillabackend.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -14,28 +12,26 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.better.foodzillabackend.user.logic.service.UserService;
 
 @Configuration
-@RequiredArgsConstructor
-public class ApplicationBeans {
-
-    @Value("${security.jwt.secret}")
-    private String jwtSecret;
+public class SecurityConfig {
 
     @Bean
-    public Algorithm jwtAlgorithm() {
-        return Algorithm.HMAC256(jwtSecret);
+    public Algorithm jwtAlgorithm(SecurityProperties properties) {
+        return Algorithm.HMAC256(properties.getTokenSecret());
     }
 
     @Bean
-    public JWTVerifier verifier(Algorithm algorithm) {
-        return JWT.require(algorithm)
-                .withIssuer("my-graphql-api")
+    public JWTVerifier verifier(SecurityProperties properties, Algorithm algorithm) {
+        return JWT
+                .require(algorithm)
+                .withIssuer(properties.getTokenIssuer())
                 .build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public PasswordEncoder passwordEncoder(SecurityProperties properties) {
+        return new BCryptPasswordEncoder(properties.getPasswordStrength());
     }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider(UserService userService, PasswordEncoder passwordEncoder) {
