ac1898538208c8c9957231f1f8b793653f264260
Damian2057 <ddamian2057@gmail.com>
2023-04-02 11:51:18 +0200

    SecurityContextHolder usage

--- src/main/java/pl/better/foodzillabackend/customer/logic/controller/CustomerController.java
+++ src/main/java/pl/better/foodzillabackend/customer/logic/controller/CustomerController.java
@@ -1,22 +1,16 @@
 package pl.better.foodzillabackend.customer.logic.controller;
 
-import graphql.GraphQLContext;
-import graphql.schema.DataFetchingEnvironment;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import pl.better.foodzillabackend.auth.service.accesstype.LoggedInUser;
 import pl.better.foodzillabackend.customer.logic.model.command.CreateCustomerCommand;
 import pl.better.foodzillabackend.customer.logic.model.command.UpdateCustomerCommand;
 import pl.better.foodzillabackend.customer.logic.model.dto.CustomerDto;
 import pl.better.foodzillabackend.customer.logic.service.CustomerService;
-
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Controller
 @RequiredArgsConstructor
@@ -31,13 +25,9 @@ public class CustomerController {
 
     @LoggedInUser
     @MutationMapping
-    public CustomerDto editCustomer(@Argument @Valid UpdateCustomerCommand customer, DataFetchingEnvironment env) {
-        SecurityContext context = (SecurityContext) env.getGraphQlContext()
-                .stream()
-                .toList()
-                .get(0)
-                .getValue();
-        String principal = (String) context.getAuthentication().getPrincipal();
+    public CustomerDto editCustomer(@Argument @Valid UpdateCustomerCommand customer) {
+        String principal = (String)
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return customerService.editCustomer(principal, customer);
     }
 }
