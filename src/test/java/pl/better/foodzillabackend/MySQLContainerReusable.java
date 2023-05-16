f584ad181596ce404c559e0cba2e5f73ad8db416
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-05-16 13:37:03 +0200

    Reusable container

---
+++ src/test/java/pl/better/foodzillabackend/MySQLContainerReusable.java
@@ -0,0 +1,33 @@
+package pl.better.foodzillabackend;
+
+import org.testcontainers.containers.MySQLContainer;
+
+public class MySQLContainerReusable extends MySQLContainer<MySQLContainerReusable> {
+    private static final String IMAGE_VERSION = "mysql:5.7";
+    private static MySQLContainerReusable container;
+
+    private MySQLContainerReusable() {
+        super(IMAGE_VERSION);
+        this.withInitScript("init.sql");
+    }
+
+    public static MySQLContainerReusable getInstance() {
+        if (container == null) {
+            container = new MySQLContainerReusable();
+        }
+        return container;
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.setProperty("TEST_MYSQL_USER", container.getUsername());
+        System.setProperty("TEST_MYSQL_PASSWORD", container.getPassword());
+        System.setProperty("TEST_MYSQL_URL", container.getJdbcUrl());
+    }
+
+    @Override
+    public void stop() {
+        //do nothing, JVM handles shut down
+    }
+}
--- src/test/java/pl/better/foodzillabackend/TestBase.java
+++ src/test/java/pl/better/foodzillabackend/TestBase.java
@@ -5,17 +5,12 @@ import org.junit.jupiter.api.BeforeAll;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.env.Environment;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
 import pl.better.foodzillabackend.customer.logic.repository.CustomerRepository;
 import pl.better.foodzillabackend.ingredient.logic.repository.IngredientRepository;
 import pl.better.foodzillabackend.recipe.logic.repository.RecipeRepositoryAdapter;
@@ -31,16 +26,7 @@ import java.io.IOException;
 public class TestBase {
 
     @Container
-    static MySQLContainer<?> mySQLContainer =
-            new MySQLContainer<>("mysql:5.7")
-                    .withInitScript("init.sql");
-
-    @DynamicPropertySource
-    static void mySqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", mySQLContainer::getUsername);
-        registry.add("spring.datasource.password", mySQLContainer::getPassword);
-    }
+    static MySQLContainer<?> mySQLContainer = MySQLContainerReusable.getInstance();
 
     @Autowired
     protected GraphQlTester graphQlTester;
