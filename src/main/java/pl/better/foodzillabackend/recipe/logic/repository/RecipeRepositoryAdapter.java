597edf56470af30e374a84ec64389fb5f74005e3
Radosław Bucki <236507@edu.p.lodz.pl>
2023-05-16 17:42:08 +0200

    Lazy loading bugfix

--- src/main/java/pl/better/foodzillabackend/recipe/logic/repository/RecipeRepositoryAdapter.java
+++ src/main/java/pl/better/foodzillabackend/recipe/logic/repository/RecipeRepositoryAdapter.java
@@ -3,15 +3,20 @@ package pl.better.foodzillabackend.recipe.logic.repository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import pl.better.foodzillabackend.exceptions.type.RecipeNotFoundException;
+import pl.better.foodzillabackend.ingredient.logic.repository.IngredientRepository;
 import pl.better.foodzillabackend.recipe.logic.mapper.RecipeDtoMapper;
 import pl.better.foodzillabackend.recipe.logic.model.domain.Recipe;
 import pl.better.foodzillabackend.recipe.logic.model.dto.RecipeDto;
 import pl.better.foodzillabackend.recipe.logic.repository.redis.RecipeTemplate;
 import pl.better.foodzillabackend.recipe.logic.repository.sql.RecipeRepository;
+import pl.better.foodzillabackend.review.logic.repository.ReviewRepository;
+import pl.better.foodzillabackend.tag.logic.repository.TagRepository;
 import pl.better.foodzillabackend.utils.rabbitmq.recipe.RecipeConsumer;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -23,6 +28,10 @@ public class RecipeRepositoryAdapter {
     private final RecipeTemplate recipeTemplate;
     private final RecipeDtoMapper recipeDtoMapper;
 
+    private final IngredientRepository ingredientRepository;
+    private final TagRepository tagRepository;
+    private final ReviewRepository reviewRepository;
+
     public Recipe findById(long id) {
         return recipeRepository.findById(id).orElseThrow(() -> new RecipeNotFoundException(
                 RECIPE_NOT_FOUND_MESSAGE.formatted(id)
@@ -30,13 +39,26 @@ public class RecipeRepositoryAdapter {
     }
 
     public RecipeDto getRecipeById(long id) {
-        return recipeTemplate.getById(id).orElse(
-                recipeRepository.getRecipeDetailsById(id)
-                        .map(recipeDtoMapper)
-                        .orElseThrow(() -> new RecipeNotFoundException(
-                                RECIPE_NOT_FOUND_MESSAGE.formatted(id)
-                        ))
-        );
+        return recipeTemplate.getById(id).orElse(recipeDtoMapper.apply(getRecipeByIdFromSql(id)));
+    }
+    protected Recipe getRecipeByIdFromSql(long id) {
+        Recipe recipe = recipeRepository.getRecipeDetailsById(id).orElseThrow(() -> new RecipeNotFoundException(
+                RECIPE_NOT_FOUND_MESSAGE.formatted(id)
+        ));
+
+        recipe.setIngredients(getRecipeItems(ingredientRepository.findAllByRecipeId(recipe.getId())));
+        recipe.setTags(getRecipeItems(tagRepository.findAllByRecipeId(recipe.getId())));
+        recipe.setReviews(getRecipeItems(reviewRepository.findAllByRecipeId(recipe.getId())));
+
+        return recipe;
+    }
+
+
+    public static <T> Set<T> getRecipeItems(List<T> items) {
+        if (items.size() == 1 && items.get(0) == null) {
+            items = List.of();
+        }
+        return new HashSet<>(items);
     }
 
     public List<RecipeDto> getRecipesByIds(List<Long> ids) {
