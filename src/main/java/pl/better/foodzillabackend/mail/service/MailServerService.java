393eade67dca0be3f534c7da418f899e200da29b
Damian2057 <ddamian2057@gmail.com>
2023-05-17 10:57:14 +0200

    initial class structure for email sending

--- src/main/java/pl/better/foodzillabackend/mail/service/MailServerService.java
+++ src/main/java/pl/better/foodzillabackend/mail/service/MailServerService.java
@@ -1,4 +1,17 @@
 package pl.better.foodzillabackend.mail.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.better.foodzillabackend.mail.model.command.ResetPasswordCommand;
+
+@Service
+@RequiredArgsConstructor
 public class MailServerService {
+    public boolean requestPasswordResetEmail(String email) {
+        return false;
+    }
+
+    public boolean resetPassword(ResetPasswordCommand command) {
+        return false;
+    }
 }
